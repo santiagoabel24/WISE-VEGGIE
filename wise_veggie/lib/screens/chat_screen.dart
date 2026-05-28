3c8a74cf3cdaea2e5713e71c4f4387c09b3a96ec
santiagoabel24 <santiago24521@cbtis75.edu.mx>
2026-05-28 09:32:17 -0600

    Reinicio total: eliminadas carpetas anteriores y subida solo wise_veggie

--- wise_veggie/lib/screens/chat_screen.dart
+++ wise_veggie/lib/screens/chat_screen.dart
@@ -8,7 +8,7 @@ class NutriBotChatScreen extends StatefulWidget {
 }
 
 class _NutriBotChatScreenState extends State<NutriBotChatScreen> {
-  final String apiKey = "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";
+  final String apiKey = "";
   
   final String systemPrompt = "Eres NutriBot, un asistente virtual amigable, empatico y confiable especializado en salud general, alimentacion saludable y recetas nutritivas. Tu conocimiento se basa en las guias de la OMS. SIEMPRE aclara que no eres medico.";
 
